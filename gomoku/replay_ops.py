1fd3c4ccd74ff842c5c67c6a5f92e5a8deb0c050
Pawe? Nowak <pawel834@gmail.com>
2016-11-12 01:34:14 +0100

    Fixed the build of replay_ops kernel

--- gomoku/replay_ops.py
+++ gomoku/replay_ops.py
@@ -1,5 +1,9 @@
 """Ops used to construct training samples from serialized replays."""
+import os.path
+
 import tensorflow as tf
 
-_replay_ops_module = tf.load_op_library('replay_ops.so')
+_replay_ops_module = tf.load_op_library(
+    os.path.join(tf.resource_loader.get_data_files_path(),
+                 'replay_ops_kernel.so'))
 decode_replays = _replay_ops_module.decode_replays
