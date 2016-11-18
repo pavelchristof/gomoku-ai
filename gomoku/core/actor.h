7679b2084f6692cc90dd44cf81fb7196a31c7940
Pawe? Nowak <pawel834@gmail.com>
2016-11-18 22:16:50 +0100

    Added a basic monte carlo actor

--- gomoku/core/actor.h
+++ gomoku/core/actor.h
@@ -5,6 +5,7 @@
 #include <memory>
 
 #include "gomoku/core/board.h"
+#include "gomoku/core/monte_carlo.h"
 
 namespace gomoku {
 
@@ -30,6 +31,47 @@ using ActorFactory = std::function<std::unique_ptr<Actor>()>;
 
 // Creates actors that always make random moves.
 ActorFactory RandomActorFactory();
+
+template <typename Cell>
+class MonteCarloActor : public Actor {
+  void GameStarted(Player me, int seed) override {
+    board_.Reset();
+    rng_.seed(seed);
+  }
+  void ObserveMove(Eigen::Vector2i move) override {
+    board_.ApplyMove(move);
+  }
+
+  Eigen::Vector2i ChooseMove(StopSignal stop_signal) override {
+    Cell cell{FeatureMatrix::Constant(0.5f), 1e-5f};
+    while (!stop_signal()) {
+      Board board = board_;
+      const Eigen::Vector2i exploring_move = SampleSoftmax(
+          cell.WinProb(), StoneMatrix(board, Player::NONE), 1.0f, &rng_);
+      board.ApplyMove(exploring_move);
+      Eigen::Vector2i move = exploring_move;
+      while (!board.DidWin(move)) {
+        move = SampleWeightMatrix(StoneMatrix(board, Player::NONE), &rng_);
+        board.ApplyMove(move);
+      }
+      Player winner = kOpponent[board.CurrentPlayer()];
+      float score = winner == board_.CurrentPlayer() ? 1.0f : 0.0f;
+      cell.Update(exploring_move, score);
+    }
+    return SampleGreedy(cell.WinProb(), StoneMatrix(board_, Player::NONE));
+  }
+
+ private:
+  Board board_;
+  std::mt19937_64 rng_;
+};
+
+template <typename Cell>
+ActorFactory MonteCarloActorFactory() {
+  return [] () -> std::unique_ptr<Actor> {
+    return std::unique_ptr<Actor>(new MonteCarloActor<Cell>());
+  };
+}
 
 }  // namespace gomoku
 
