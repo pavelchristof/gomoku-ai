bdfa3ebc7c3102b69d6fdd6170e88f2833e4d379
Pawe? Nowak <pawel834@gmail.com>
2016-11-24 21:51:49 +0100

    Added a worker binary

---
+++ gomoku/worker.cc
@@ -0,0 +1,28 @@
+#include "gflags/gflags.h"
+#include "gomoku/supervisor/worker_impl.h"
+#include "grpc++/grpc++.h"
+#include "tensorflow/core/platform/logging.h"
+
+DEFINE_string(worker_address, "unix:/tmp/gomoku_worker",
+              "Address of the gomoku worker.");
+
+namespace gomoku {
+
+void RunWorker() {
+  WorkerImpl worker;
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(FLAGS_worker_address,
+                           grpc::InsecureServerCredentials());
+  builder.RegisterService(&worker);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  LOG(INFO) << "Server listening on " << FLAGS_worker_address;
+  server->Wait();
+}
+
+}  // namespace gomoku
+
+int main(int argc, char **argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  gomoku::RunWorker();
+  return 0;
+}
